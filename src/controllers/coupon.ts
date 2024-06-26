31e9689793bcbb05b0bd56c8a529fb2678a79fa0
jstkiddin <zelenskaya.nika@gmail.com>
2024-06-26 18:46:10 +0300

    test

--- src/controllers/coupon.ts
+++ src/controllers/coupon.ts
@@ -2,6 +2,8 @@ import express from 'express'
 
 export const create = (req: express.Request, res: express.Response) => {
   try {
+    const { email } = req.body
+    return res.sendStatus(200).json(email).end()
   } catch (error) {
     console.log(error)
     return res.sendStatus(400)
